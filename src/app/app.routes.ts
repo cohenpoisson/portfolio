280421539d27a46c233f0fead83445ca24e61d16
cohen poisson <w0866195@myscc.ca>
2025-11-07 14:34:41 -0500

    made home better

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { RouterModule } from '@angular/router';
+import {Home} from './home/home';
+import {About} from './about/about';
+import {Contact} from './contact/contact';
+import {Projects} from './projects/projects';
+import {PageNotFound} from './page-not-found/page-not-found';
+// every import being neat like that was not intended actually so cool
+export const routes: Routes = [
+  {path: 'home', component:Home },
+  {path: 'about', component:About },
+  {path: 'contact', component:Contact },
+  {path: 'projects', component:Projects },
+  {path: '**', component: PageNotFound}
+];
---
+++ src/app/home/home.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    RouterLink,
+    NgOptimizedImage
+  ],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  spinText(event: Event) {
+    const element = event.target as HTMLElement;
+    element.classList.add('spinning');
+
+    setTimeout(() => {
+      element.classList.remove('spinning');
+    }, 600);
+  }
+}
